4f4047c213f59f6ea26244dffcfef84289d4c93c
zSat2 <satyanshchauhan553@gmail.com>
2026-01-06 23:22:37 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/DingDong/Ding/Dong/Tracker/DingDongTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.DingDong.Ding.Dong.Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingDongTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DingDongTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DingDong/Ding/Dong/Tracker/DingDongTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DingDong.Ding.Dong.Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingDongTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
